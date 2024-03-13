313e01f125badeaebe4f6fb03f0f2299dfa3c3e5
kevincito27 <kevinpinzon80@gmail.com>
2024-03-13 11:51:17 -0500

    ARREGLO SLIDE Y TOUR
    AGREGO IMAGENES Y CORRIJO PAG TOUR

--- src/pages/Galeria.tsx
+++ src/pages/Galeria.tsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
+import image1 from '../img/CEREBRO.png';
+import image2 from '../img/pierna.jpg';
+import image3 from '../img/pulmones.jpg';
+import image4 from '../img/CRANEO.jpg';
+import image5 from '../img/bloque diafragmatico.jpg';
 
 const Galeria = () => {
   const slides = [
     {
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+      url: image1,
     },
     {
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+      url: image2,
     },
     {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+      url: image3,
     },
     {
-      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+      url: image4,
     },
     {
-      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+      url: image5,
     },
   ];
 
@@ -35,7 +40,7 @@ const Galeria = () => {
     setCurrentIndex(newIndex);
   };
 
-  const goToSlide = (slideIndex: number) => { 
+  const goToSlide = (slideIndex) => { 
     setCurrentIndex(slideIndex);
   };
 
@@ -48,11 +53,13 @@ const Galeria = () => {
           </h2> 
         </div>
 
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-        >
-        </div>
+        <img
+          src={slides[currentIndex].url}
+          alt="slide"
+          className='w-full h-full rounded-2xl object-contain'
+          style={{ maxWidth: '100%', maxHeight: '100%' }}
+
+        />
 
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
           <BsChevronCompactLeft onClick={prevSlide} size={30} />
@@ -75,7 +82,6 @@ const Galeria = () => {
 
       {/* Espacio entre el slide y el párrafo */}
       <div className="mt-8"></div>
-
       {/* Información adicional debajo del slide */}
       <div className="text-center">
         <p>nico esta muy lindo </p>
--- src/pages/Tour.tsx
+++ src/pages/Tour.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import recorrido from '../img/UMNG-Light 1.png';
-
+import recorrido from '../img/PANTALLA RECORRIDO.png';
 
 const Tour = () => {
   return (
-    <div className='h-screen flex  justify-center bg-cover bg-no-repeat' style={{ backgroundImage: `url(${recorrido})` }}>
-          <div className='text-center text-blue-950 capitalize' >
-                        <h1 className='text-4xl py-12 font-extrabold'>
-              RECORRIDO VIRTUAL <br />
-          </h1>
-        
-              <div className='flex items-center justify-around py-5 '>
-                  <button className='bg-white text-blue-950 py-2 px-4 rounded-full font-bold ' type="button"> COMENCEMOS</button>
-                
-              </div>
-                   
+    <div className='h-screen flex justify-center'>
+      <div className='text-center text-blue-950 capitalize relative'>
+        <h1 className='text-4xl py-12 font-extrabold'>
+          RECORRIDO VIRTUAL <br />
+        </h1>
+        <div className='relative my-6' >
+          <img src={recorrido} className='max-w-none max-h-none opacity-90' alt="" />
+          <div className='absolute inset-0 flex items-center justify-center'>
+            <button className='bg-white text-blue-950 py-2 px-4 rounded-full font-bold' type="button">COMENCEMOS</button>
           </div>
-
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Tour
-
+export default Tour;
