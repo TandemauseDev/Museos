93fc394d7026af754ec894d471a6b5cbdddf26df
kevincito27 <kevinpinzon80@gmail.com>
2024-03-13 16:52:21 -0500

    TITULOS

--- src/pages/Galeria.tsx
+++ src/pages/Galeria.tsx
@@ -11,18 +11,23 @@ const Galeria = () => {
   const slides = [
     {
       url: image1,
+      text: "SISTEMA NERVIOSO CENTRAL"
     },
     {
       url: image2,
+      text: "EXTREMIDAD INFERIOR"
     },
     {
       url: image3,
+      text: "SISTEMA RESPIRATORIO"
     },
     {
       url: image4,
+      text: "CRANEO "
     },
     {
       url: image5,
+      text: "BLOQUE DIAFRAGMATICO"
     },
   ];
 
@@ -45,10 +50,10 @@ const Galeria = () => {
   };
 
   return (
-    <div>
-      <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+    <div style={{ maxHeight: 'calc(100vh - 100px)', overflowY: 'auto' }}>
+      <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px- relative group '>
         <div className='text-center text-blue-950 decoration-wavy'> 
-          <h2 className='text-4xl py-4'>
+          <h2 className='text-4xl py-0.5 font-extrabold'>
             EXPOSICION ANATOMICA <br />
           </h2> 
         </div>
@@ -58,7 +63,6 @@ const Galeria = () => {
           alt="slide"
           className='w-full h-full rounded-2xl object-contain'
           style={{ maxWidth: '100%', maxHeight: '100%' }}
-
         />
 
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
@@ -72,7 +76,7 @@ const Galeria = () => {
             <div
               key={slideIndex}
               onClick={() => goToSlide(slideIndex)}
-              className='text-2xl cursor-pointer'
+              className={`text-2xl cursor-pointer ${slideIndex === currentIndex ? 'text-blue-950' : 'text-gray-400'}`}
             >
               <RxDotFilled />
             </div>
@@ -81,10 +85,11 @@ const Galeria = () => {
       </div>
 
       {/* Espacio entre el slide y el párrafo */}
-      <div className="mt-8"></div>
+      <div className="mt-4"></div>
+      <div className='pd-2'></div>
       {/* Información adicional debajo del slide */}
-      <div className="text-center">
-        <p>nico esta muy lindo </p>
+      <div className="text-center text-2xl py-2 font-bold">
+        <p>{slides[currentIndex].text}</p>
       </div>
     </div>
   );
